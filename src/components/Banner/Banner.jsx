875b49432adf81939889c6dc2401feb28e781fb8
mdrifatulislam <mdrifatulislam1080@gmail.com>
2025-10-08 13:59:21 +0600

    added some files and folder from hero-app projects

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="text-center my-15">
+      <h1 className="text-7xl font-bold">
+        We Build <br />
+        <span className="bg-gradient-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent">
+          Productive{" "}
+        </span>
+        Apps
+      </h1>
+      <p className="my-7 text-[#627382]">
+        At HERO.IO , we craft innovative apps designed to make everyday life
+        simpler, smarter, and more exciting. <br /> Our goal is to turn your
+        ideas into digital experiences that truly make an impact.
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
